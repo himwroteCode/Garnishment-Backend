160cbe99454338cc8a8e3badffcd002df49ce287
Ritik Jaiswal <ritikjaiswal8888@gmail.com>
2024-06-13 00:41:10 +0530

    Version3-13

--- User_app/views.py
+++ User_app/views.py
@@ -780,13 +780,13 @@ def CalculationDataView(request):
     if request.method == 'POST':
         try:
             data = json.loads(request.body)
-            required_fields = ['employee_name','earning','taxes','FIT','social','medicare','state','garnishment_fees' ,'arrears_greater_than_12_weeks','minimun_wage','total_amount_to_withhold']
+            required_fields = ['employee_name', 'earning', 'taxes', 'FIT', 'social', 'medicare', 'state', 'garnishment_fees', 'arrears_greater_than_12_weeks', 'minimun_wage', 'total_amount_to_withhold']
             missing_fields = [field for field in required_fields if field not in data or not data[field]]
             if missing_fields:
-                return JsonResponse({'error': f'Required fields are missing: {", ".join(missing_fields)}','status_code':status.HTTP_400_BAD_REQUEST})         
+                return JsonResponse({'error': f'Required fields are missing: {", ".join(missing_fields)}'}, status=status.HTTP_400_BAD_REQUEST)         
             user = Gcalculation_data.objects.create(**data)
-            return JsonResponse({'message': 'Calculations Details Successfully Registered', status:status.HTTP_201_CREATED})
+            return JsonResponse({'message': 'Calculations Details Successfully Registered'}, status=status.HTTP_201_CREATED)
         except Exception as e:
-            return JsonResponse({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR) 
+            return JsonResponse({'error': str(e), status:status.HTTP_500_INTERNAL_SERVER_ERROR}) 
     else:
-        return JsonResponse({'message': 'Please use POST method','status_code':status.HTTP_400_BAD_REQUEST})
+        return JsonResponse({'message': 'Please use POST method', status:status.HTTP_400_BAD_REQUEST})
