0a8b11ccda5b1e469b1d9748c439b803050ac248
Ritik Jaiswal <ritikjaiswal8888@gmail.com>
2024-09-16 16:03:42 +0530

    v-2

--- auth_project/wsgi.py
+++ auth_project/wsgi.py
@@ -9,8 +9,6 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'auth_project.settings')
 
 application = get_wsgi_application()
