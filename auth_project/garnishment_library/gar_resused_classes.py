fa1f6b7a14b8c095794704c5cfa96aa242d2cf7d
Ritik Jaiswal <ritikjaiswal8888@gmail.com>
2024-12-30 12:04:29 +0530

    Added WL limit logic

--- auth_project/garnishment_library/gar_resused_classes.py
+++ auth_project/garnishment_library/gar_resused_classes.py
@@ -71,4 +71,49 @@ class CalculateArrearAmountForChild:
         elif self.amount_left_for_arrears > 0:
             return self.amount_left_for_arrears / self.number_of_arrear
         else:
-            return 0
+            return 0        
+
+class WLIdentifier:
+    def __init__(self, state,DE,employee_id, rule_name, supports_2nd_family, arrears_of_more_than_12_weeks, de_gt_145,order_gt_one):
+        self.state = state.lower() 
+        self.DE = DE.lower()
+        self.employee_id = employee_id.lower() 
+        self.rule_name = rule_name.lower()
+        self.supports_2nd_family = supports_2nd_family.lower()
+        self.arrears_of_more_than_12_weeks = arrears_of_more_than_12_weeks.lower()
+        self.de_gt_145 = de_gt_145.lower()
+        self.order_gt_one = order_gt_one.lower()
+    
+    def get_state_rules(self):
+        file_path = os.path.join(settings.BASE_DIR, 'User_app', 'withholding_rules.json')
+
+        # Reading the JSON file
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+        # Accessing child support data
+        ccpa_rules_data = data.get("state", [])
+        
+        # Searching for the matching state
+        for record in ccpa_rules_data:
+            if record['State'].lower() == self.state:
+                return record['Rule']         
+        # If no matching record is found
+        return f"No allocation method found for the state: {self.state.capitalize()}."
+    
+    def find_wl_value(self,state_rule):
+        file_path = os.path.join(settings.BASE_DIR, 'User_app', 'withholding_limits.json')
+        
+        # Reading the JSON file
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+        # Accessing child support data
+        ccpa_limits_data = data.get("Rules", [])
+        for rule in ccpa_limits_data["Rules"]:
+            if rule["Rule"] == self.get_state_rules(state_rule):
+                for detail in rule["Details"]:
+                    if  (detail["Supports_2nd_family"] == "" and detail["Arrears_of_more_than_12_weeks"] == "")  or (detail["Supports_2nd_family"] == self.supports_2nd_family 
+                        and detail["Arrears_of_more_than_12_weeks"] == self.arrears_of_more_than_12_weeks 
+                        and detail["de_gt_145"]==self.de_gt_145 and detail["order_gt_one"]==self.order_gt_one) :
+                        return detail["WL"]
+        return(f"No matching WL found for the this employee:{self.employee_id}")
+
